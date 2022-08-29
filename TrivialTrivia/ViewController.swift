7ee7cfe95772556c7586df45ac57bdeb46ea856c
Hima Patel <patelhiima@gmail.com>
2022-08-29 17:23:50 -0600

    Initial Commit

---
+++ TrivialTrivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrivialTrivia
+//
+//  Created by Hima Patel on 2022-08-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
